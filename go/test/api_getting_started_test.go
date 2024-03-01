869494ae12b90fe4af8376f36e1b39d501a5f484
dphuang2 <dylan@konfigthis.com>
2024-03-01 13:10:23 -0800

    format

--- go/test/api_getting_started_test.go
+++ go/test/api_getting_started_test.go
@@ -1,29 +1,31 @@
 package carbon
 
 import (
-    "testing"
-    "github.com/stretchr/testify/assert"
-    "github.com/stretchr/testify/require"
-    carbon "github.com/Carbon-for-Developers/carbon-sdks/go"
+	"testing"
+
+	carbon "github.com/Carbon-for-Developers/carbon-sdks/go"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_carbon_(t *testing.T) {
 
-    configuration := carbon.NewConfiguration()
-    configuration.SetHost("http://127.0.0.1:4010")
-    configuration.SetAccessToken("AUTHORIZATION")
-    configuration.SetApiKey("AUTHORIZATION")
-    configuration.SetCustomerId("CUSTOMER_ID")
-    client := carbon.NewAPIClient(configuration)
+	configuration := carbon.NewConfiguration()
+	configuration.SetHost("http://127.0.0.1:4010")
+	configuration.SetAccessToken("AUTHORIZATION")
+	configuration.SetApiKey("AUTHORIZATION")
+	configuration.SetCustomerId("CUSTOMER_ID")
+	client := carbon.NewAPIClient(configuration)
 
-    t.Run("Test GettingStarted GetAccessToken", func(t *testing.T) {
-        request := client.AuthApi.GetAccessToken()
+	t.Run("Test GettingStarted GetAccessToken", func(t *testing.T) {
+		request := client.AuthApi.GetAccessToken()
+		client.AuthApi.GetAccessToken().Execute()
 
-        resp, httpRes, err := request.Execute()
+		resp, httpRes, err := request.Execute()
 
-        require.Nil(t, err)
-        require.NotNil(t, resp)
-        assert.Equal(t, 200, httpRes.StatusCode)
-    })
+		require.Nil(t, err)
+		require.NotNil(t, resp)
+		assert.Equal(t, 200, httpRes.StatusCode)
+	})
 
 }
