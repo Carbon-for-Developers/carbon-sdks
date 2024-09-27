754bd40ab488742f8255d5b5b380d93b58fb4a43
konfig-publisher <publisher@konfigthis.com>
2024-09-27 04:37:31 +0000

    Version Bump SDKs

--- python/carbon/api_client.py
+++ python/carbon/api_client.py
@@ -1514,7 +1514,7 @@ class ApiClient:
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'Konfig/0.2.37/python'
+        self.user_agent = 'Konfig/0.2.38/python'
 
     def __enter__(self):
         return self
