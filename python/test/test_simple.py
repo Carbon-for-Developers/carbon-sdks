17ee0fb8be39e3c688d9fbe1f95a18915a720883
dphuang2 <dylan@konfigthis.com>
2024-11-19 13:01:17 -0800

    add test_white_label_create

--- python/test/test_simple.py
+++ python/test/test_simple.py
@@ -15,40 +15,38 @@ import os
 from pprint import pprint
 from carbon import Carbon
 
+
 class TestSimple(unittest.TestCase):
     def setUp(self):
         pass
 
     def test_integer_arg(self):
         carbon = Carbon(
-            api_key = 'YOUR_API_KEY',
-            customer_id = 'YOUR_API_KEY',
-            host = "http://127.0.0.1:4010"
+            api_key="YOUR_API_KEY",
+            customer_id="YOUR_API_KEY",
+            host="http://127.0.0.1:4010",
         )
-        res = carbon.integrations.get_oauth_url( 
-            service='SHAREPOINT',
+        res = carbon.integrations.get_oauth_url(
+            service="SHAREPOINT",
             data_source_id=int(17714),
-            request_id='{request_id: 123}',
+            request_id="{request_id: 123}",
             skip_embedding_generation=True,
-            microsoft_tenant='modelml',
-            sharepoint_site_name='word-ingestion-dev',
+            microsoft_tenant="modelml",
+            sharepoint_site_name="word-ingestion-dev",
             enable_file_picker=False,
         )
         self.assertIsNotNone(res)
 
     def test_auth_schemes(self):
         carbon = Carbon(
-            api_key = 'YOUR_API_KEY',
-            customer_id = 'YOUR_API_KEY',
-            host = "http://127.0.0.1:4010"
+            api_key="YOUR_API_KEY",
+            customer_id="YOUR_API_KEY",
+            host="http://127.0.0.1:4010",
         )
         token = carbon.auth.get_access_token()
         self.assertIsNotNone(token)
         pprint(token)
-        carbon = Carbon(
-            access_token = token.access_token,
-            host = "http://127.0.0.1:4010"
-        )
+        carbon = Carbon(access_token=token.access_token, host="http://127.0.0.1:4010")
         self.assertIsNotNone(carbon)
         white_labeling = carbon.auth.get_white_labeling()
         self.assertIsNotNone(white_labeling)
@@ -56,9 +54,9 @@ class TestSimple(unittest.TestCase):
 
     def test_various_endpoints(self):
         carbon = Carbon(
-            api_key = 'YOUR_API_KEY',
-            customer_id = 'YOUR_API_KEY',
-            host = "http://127.0.0.1:4010"
+            api_key="YOUR_API_KEY",
+            customer_id="YOUR_API_KEY",
+            host="http://127.0.0.1:4010",
         )
         data_sources = carbon.data_sources.query_user_data_sources(
             pagination={
@@ -74,28 +72,50 @@ class TestSimple(unittest.TestCase):
         pprint(data_sources)
         self.assertIsNotNone(data_sources)
 
-        files = carbon.integrations.sync_s3_files(ids=[{"id": "ID", "bucket": "BUCKET"}])
+        files = carbon.integrations.sync_s3_files(
+            ids=[{"id": "ID", "bucket": "BUCKET"}]
+        )
         pprint(files)
         self.assertIsNotNone(files)
 
         file = carbon.files.upload(
-            file=open("README.md", "rb"),
-            embedding_model="OPENAI"
+            file=open("README.md", "rb"), embedding_model="OPENAI"
         )
         pprint(file)
         self.assertIsNotNone(file)
 
     def test_datetime_deserialization(self):
         from datetime import datetime
+
         carbon = Carbon(
-            api_key='YOUR_API_KEY',
-            customer_id='YOUR_API_KEY',
-            host="http://127.0.0.1:4010"
+            api_key="YOUR_API_KEY",
+            customer_id="YOUR_API_KEY",
+            host="http://127.0.0.1:4010",
         )
         res = carbon.integrations.list_data_source_items(data_source_id=4)
         self.assertIsNotNone(res)
         self.assertTrue(len(res.items) > 0)
         self.assertTrue(type(res.items[0].updated_at) is datetime)
+
+    async def test_white_label_create(self):
+        carbon = Carbon(
+            api_key="YOUR_API_KEY",
+            customer_id="YOUR_API_KEY",
+            host="http://127.0.0.1:4010",
+        )
+        res = await carbon.white_label.acreate(
+            body=[
+                {
+                    "data_source_type": "SALESFORCE",
+                    "credentials": {
+                        "client_id": "CLIENT_ID",
+                        "client_secret": "CLIENT_SECRET",
+                        "redirect_uri": f"https://carbon.test.app/integrations/SALESFORCE",
+                    },
+                }
+            ],
+        )
+        self.assertIsNotNone(res)
 
     def tearDown(self):
         pass
